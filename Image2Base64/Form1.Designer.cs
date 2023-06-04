c9136dc842a94afcc2713197c658eabd3db0af8e
Patbodin Intajak <patbodin30443@hotmail.com>
2023-06-04 18:00:24 +0700

    Add icon for app

--- Image2Base64/Form1.Designer.cs
+++ Image2Base64/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace Image2Base64
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.txtFilePath = new System.Windows.Forms.TextBox();
             this.btnBrowse = new System.Windows.Forms.Button();
@@ -252,6 +253,7 @@ namespace Image2Base64
             this.Controls.Add(this.txtFilePath);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
